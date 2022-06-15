a760a93a282c9ee69500f4b7563ab7ab1b9ad466
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-06-15 21:50:31 +0200

    Suppression de react-dom/client

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOMClient from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import ReactDOM from "react-dom";
 
 
-const root = ReactDOMClient.createRoot(document.getElementById('root'));
-root.render(
-   <React.StrictMode>
-     <App />
-   </React.StrictMode>
- );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
