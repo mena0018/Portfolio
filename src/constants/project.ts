a78729755a97f6c9b09f69ed72c4b57d5f966e0b
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-08-28 10:39:55 +0200

    Ajout du projet Landing-Site

--- src/constants/project.ts
+++ src/constants/project.ts
@@ -2,25 +2,30 @@ import { ProjectType } from '../types/project';
 
 import WebgestionV2 from "../img/webp/projects/webgestion-v2.webp";
 import CountDown from "../img/webp/projects/countdown.webp";
-import TodoListPWA from "../img/webp/projects/todolist-pwa.webp";
+import TradingSite from "../img/webp/projects/trading-site.webp";
 import Ecommerce from "../img/webp/projects/ecommerce.webp";
 
 import javascript from "../img/icones/langages/javascript.svg";
 import typescript from "../img/icones/langages/typescript.svg";
 import react from "../img/icones/langages/react.svg";
 import sass from "../img/icones/langages/sass.svg";
-import pwa from "../img/icones/langages/pwa.svg";
 import symfony from "../img/icones/langages/symfony.svg";
 import mysql from "../img/icones/langages/mysql.svg";
 
 
 let css  = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg";
-let html = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg";
 let redux = "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg";
 
 
 export const dataProjects: Record<string, ProjectType[]> = {
   FR: [
+    {
+      title: "Trading-Site",
+      description: "Landing Page d'un site de trading réalisé dans le cadre d'un challenge Frontend-Mentor.",
+      img: TradingSite,
+      url: ["https://github.com/mena0018/Trading-Website", "https://trading-website-five.vercel.app"],
+      icons:  [ typescript, react, sass]
+    },
     {
       title: "CountDown-Timer",
       description: "Compte à rebours réalisé dans le cadre d'un challenge Frontend-Mentor.",
@@ -41,16 +46,16 @@ export const dataProjects: Record<string, ProjectType[]> = {
       img: Ecommerce,
       url: ["https://github.com/mena0018/E-commerce"],
       icons:  [ javascript, react, redux, sass ]
-    },
-    {
-      title: "TodoList PWA",
-      description: "Permet de se synchroniser avec plusieurs appareils et dispose d'un mode hors ligne.",
-      img: TodoListPWA,
-      url: ["https://github.com/mena0018/TodoList-Progressive-Web-App"],
-      icons:  [ html, css, javascript, pwa ]
     }
   ],
   EN: [
+    {
+      title: "Trading-Site",
+      description: "Landing Page challenge proposed by Frontend-Mentor.",
+      img: TradingSite,
+      url: ["https://github.com/mena0018/Trading-Website", "trading-website-five.vercel.app/"],
+      icons:  [ typescript, react, sass]
+    },
     {
       title: "CountDown-Timer",
       description: "Countdown challenge proposed by Frontend Mentor.",
@@ -71,14 +76,6 @@ export const dataProjects: Record<string, ProjectType[]> = {
       img: Ecommerce,
       url: ["https://github.com/mena0018/E-commerce"],
       icons:  [ javascript, react, redux, sass ]
-    },
-    {
-      title: "TodoList PWA",
-      description:
-        "Allows you to synchronize with multiple devices and has an offline mode.",
-      img: TodoListPWA,
-      url: ["https://github.com/mena0018/TodoList-Progressive-Web-App"],
-      icons:  [ html, css, javascript, pwa ]
     }
   ]
 };
