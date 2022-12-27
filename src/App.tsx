2367fffa1c0112bfb43ec13c34f3938467e999df
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-12-27 15:57:41 +0100

    Modification du loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from "react";
-import Navbar from "./components/Navbar/Navbar";
-import ToggleLang from "./components/ToggleLangs.js/ToggleLang";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Skills from "./components/Skills/Skills";
-import ProjectList from "./components/Project/ProjectList";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
-import ContextProvider from "./context/langContext";
-import ThemeContextProvider from "./context/themeContext";
-import Loader from "./components/Loader/Loader";
+import { useState, useEffect } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import ToggleLang from './components/ToggleLangs.js/ToggleLang';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import ProjectList from './components/Project/ProjectList';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import ContextProvider from './context/langContext';
+import ThemeContextProvider from './context/themeContext';
+import Loader from './components/Loader/Loader';
 
 function App() {
   const [loader, setLoader] = useState<boolean | null>(null);
@@ -18,7 +18,7 @@ function App() {
     setLoader(true);
     const timer = setTimeout(() => {
       setLoader(false);
-    }, 3000);
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
