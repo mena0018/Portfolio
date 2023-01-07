114a21a04e11cc76e1651f57b96bd071d6aa4c92
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2023-01-07 12:24:03 +0100

    Ajout d'un nouveau projet à la liste de projets

--- src/constants/project.ts
+++ src/constants/project.ts
@@ -1,101 +1,103 @@
-import { ProjectType } from "../types/project";
+import { ProjectType } from '../types/project';
 
-import URLShortening from "../img/webp/projects/url-shortening.webp";
-import CountDown from "../img/webp/projects/countdown.webp";
-import TradingSite from "../img/webp/projects/trading-site.webp";
-import PaymentForm from "../img/webp/projects/payment-form.webp";
+import URLShortening from '../img/webp/projects/url-shortening.webp';
+import CountDown from '../img/webp/projects/countdown.webp';
+import TradingSite from '../img/webp/projects/trading-site.webp';
+import Metaversus from '../img/webp/projects/metaversus.webp';
 
-import typescript from "../img/icones/langages/typescript.svg";
-import react from "../img/icones/langages/react.svg";
-import sass from "../img/icones/langages/sass.svg";
+import typescript from '../img/icones/langages/typescript.svg';
+import react from '../img/icones/langages/react.svg';
+import next from '../img/icones/langages/next-js.svg';
+import sass from '../img/icones/langages/sass.svg';
+import tailwind from '../img/icones/langages/tailwindcss.svg';
 
 export const dataProjects: Record<string, ProjectType[]> = {
   FR: [
     {
-      title: "Compte à Rebours",
+      title: 'Metaversus',
       description:
-        "Compte à rebours réalisé dans le cadre d'un challenge Frontend-Mentor.",
-      img: CountDown,
+        "Landing Page sur le Metaverse réalisé dans le cadre d'un challenge Frontend-Mentor",
+      img: Metaversus,
       url: [
-        "https://github.com/mena0018/Launch-Countdown-timer",
-        "https://launch-countdown-time.netlify.app",
+        'https://github.com/mena0018/Next-Metaverse',
+        'https://next-metaverse-theta.vercel.app/',
       ],
-      icons: [typescript, react, sass],
+      icons: [typescript, next, tailwind],
     },
     {
-      title: "Payment Form",
-      description:
-        "Formulaire de paiement interactif réalisé dans le cadre d'un challenge Frontend-Mentor.",
-      img: PaymentForm,
-      url: [
-        "https://github.com/mena0018/CreditCard",
-        "https://credit-card-theta.vercel.app/",
-      ],
-      icons: [typescript, react, sass],
-    },
-    {
-      title: "Trading-Site",
+      title: 'Trading-Site',
       description:
         "Landing Page d'un site de trading réalisé dans le cadre d'un challenge Frontend-Mentor.",
       img: TradingSite,
       url: [
-        "https://github.com/mena0018/Trading-Website",
-        "https://trading-website-five.vercel.app",
+        'https://github.com/mena0018/Trading-Website',
+        'https://trading-website-five.vercel.app',
       ],
       icons: [typescript, react, sass],
     },
     {
-      title: "URL Shortening",
+      title: 'URL Shortening',
       description:
         "Application de raccourcissement d'URL réalisé dans le cadre d'un challenge Frontend-Mentor.",
       img: URLShortening,
       url: [
-        "https://github.com/mena0018/URL-Shortening",
-        "https://url-shortening-virid.vercel.app/",
+        'https://github.com/mena0018/URL-Shortening',
+        'https://url-shortening-virid.vercel.app/',
+      ],
+      icons: [typescript, react, sass],
+    },
+    {
+      title: 'Compte à Rebours',
+      description:
+        "Compte à rebours réalisé dans le cadre d'un challenge Frontend-Mentor.",
+      img: CountDown,
+      url: [
+        'https://github.com/mena0018/Launch-Countdown-timer',
+        'https://launch-countdown-time.netlify.app',
       ],
       icons: [typescript, react, sass],
     },
   ],
   EN: [
     {
-      title: "CountDown-Timer",
-      description: "Countdown challenge proposed by Frontend Mentor.",
-      img: CountDown,
-      url: [
-        "https://github.com/mena0018/Launch-Countdown-timer",
-        "https://launch-countdown-time.netlify.app",
-      ],
-      icons: [typescript, react, sass],
-    },
-    {
-      title: "Payment Form",
+      title: 'Metaversus',
       description:
-        "Front-end Mentor Challenge focused on an Interactive payment form.",
-      img: PaymentForm,
+        'Frontend Mentor challenge focus on a Metaverse Landing Page.',
+      img: Metaversus,
       url: [
-        "https://github.com/mena0018/CreditCard",
-        "https://credit-card-theta.vercel.app/",
+        'https://github.com/mena0018/Next-Metaverse',
+        'https://next-metaverse-theta.vercel.app/',
       ],
-      icons: [typescript, react, sass],
+      icons: [typescript, next, tailwind],
     },
     {
-      title: "Trading-Site",
-      description: "Landing Page challenge proposed by Frontend-Mentor.",
+      title: 'Trading-Site',
+      description: 'Landing Page challenge proposed by Frontend-Mentor.',
       img: TradingSite,
       url: [
-        "https://github.com/mena0018/Trading-Website",
-        "https://trading-website-five.vercel.app/",
+        'https://github.com/mena0018/Trading-Website',
+        'https://trading-website-five.vercel.app/',
       ],
       icons: [typescript, react, sass],
     },
     {
-      title: "URL Shortening",
+      title: 'URL Shortening',
       description:
-        "Frontend Mentor challenge focus on the integration of a shortened URLs with the shrtcode API.",
+        'Frontend Mentor challenge focus on the integration of a shortened URLs with the shrtcode API.',
       img: URLShortening,
       url: [
-        "https://github.com/mena0018/URL-Shortening",
-        "https://url-shortening-virid.vercel.app/",
+        'https://github.com/mena0018/URL-Shortening',
+        'https://url-shortening-virid.vercel.app/',
+      ],
+      icons: [typescript, react, sass],
+    },
+    {
+      title: 'CountDown-Timer',
+      description: 'Countdown challenge proposed by Frontend Mentor.',
+      img: CountDown,
+      url: [
+        'https://github.com/mena0018/Launch-Countdown-timer',
+        'https://launch-countdown-time.netlify.app',
       ],
       icons: [typescript, react, sass],
     },
