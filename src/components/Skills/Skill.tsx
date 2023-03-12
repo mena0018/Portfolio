7dd311c52f98dfd19c290e7e2d1fe1f51d7fa7d2
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2023-03-12 19:18:42 +0100

    Création d'une bordure animée au survol des compétences

--- src/components/Skills/Skill.tsx
+++ src/components/Skills/Skill.tsx
@@ -6,27 +6,31 @@ import { fadeIn } from '../../utils/motion';
 
 export default function Skill({ name, stacks }: SkillType) {
   return (
-    <div className='container-skill'>
-      <motion.h2 className='subtitle' variants={fadeIn('down', 'tween', 0, 1)}>
-        {name}
-      </motion.h2>
-      <ul className='list-skills'>
-        {stacks.map((item: string, index: number) => (
-          <li key={index} className='item-skill'>
-            <motion.div
-              className='content-stack'
-              variants={fadeIn('down', 'tween', 0.1 * index, 1)}
-            >
-              <div>
-                <BsFillPatchCheckFill />
-              </div>
-              <div className='langage-level'>
-                <p>{item}</p>
-              </div>
-            </motion.div>
-          </li>
-        ))}
-      </ul>
+    <div className='wrapper'>
+      <div className='border-gradient' />
+
+      <div className='container-skill'>
+        <motion.h2 className='subtitle' variants={fadeIn('down', 'tween', 0, 1)}>
+          {name}
+        </motion.h2>
+        <ul className='list-skills'>
+          {stacks.map((item: string, index: number) => (
+            <li key={index} className='item-skill'>
+              <motion.div
+                className='content-stack'
+                variants={fadeIn('down', 'tween', 0.1 * index, 1)}
+              >
+                <div>
+                  <BsFillPatchCheckFill />
+                </div>
+                <div className='langage-level'>
+                  <p>{item}</p>
+                </div>
+              </motion.div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
