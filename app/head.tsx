5dea887c579990d2996535bdc73bc6c770e0c0ea
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2023-03-12 19:18:27 +0100

    Ajout d'un pack de favicon

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,6 @@ import Script from 'next/script';
 export default function Head() {
   return (
     <>
-      <link rel='icon' href='/favicon.png' />
       <link rel='preconnect' href='https://github.com/mena0018' />
       <link rel='preconnect' href='https://www.linkedin.com/in/rabie-menad/' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
@@ -14,7 +13,15 @@ export default function Head() {
         name='description'
         content='Salut, je suis Rabie Menad, développeur web full stack en alternance travaillant essentiellement avec React.js / TypeScript pour le côté front-end et Symfony / API Platform pour le côté back-end.'
       />
-      <link rel='apple-touch-icon' href='/favicon.png' />
+
+      <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+      <link rel='manifest' href='/site.webmanifest' />
+      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+      <meta name='msapplication-TileColor' content='#da532c' />
+      <meta name='theme-color' content='#ffffff' />
+
       <meta property='og:type' content='website' />
       <meta property='og:url' content='https://portfolio-rabie.web.app/' />
       <meta property='og:title' content='Rabie Menad • Développeur' />
