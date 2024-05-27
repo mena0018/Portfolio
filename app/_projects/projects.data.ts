89b2f6fcc3d58a8ca14475b7c4bf5a3a82b1e7b9
Rabie Menad <rabie.menad@ideine.fr>
2024-05-27 19:44:01 +0200

    🚀 add new project on project's list

--- app/_projects/projects.data.ts
+++ app/_projects/projects.data.ts
@@ -2,17 +2,19 @@ import CountDown from '@/public/images/projects/countdown.webp';
 import Metaversus from '@/public/images/projects/metaversus.webp';
 import TradingSite from '@/public/images/projects/trading-site.webp';
 import URLShortening from '@/public/images/projects/url-shortening.webp';
+import FilterSystem from '@/public/images/projects/filter-system.webp';
 
 export enum ProjectTitle {
   Metaversus = 'Metaversus',
   Trading = 'Trading-Site',
   URLShortening = 'URL Shortening',
   CountDown = 'CountDown',
+  FilterSystem = 'FilterSystem',
 }
 
 export type Project = {
+  id: string;
   link: string;
-  id: number;
   imgUrl: string;
   title: ProjectTitle;
   description: string;
@@ -20,21 +22,28 @@ export type Project = {
 
 export const projects: Array<Project> = [
   {
-    id: Date.now(),
+    id: crypto.randomUUID(),
+    imgUrl: FilterSystem.src,
+    title: ProjectTitle.FilterSystem,
+    link: 'https://filter-system-iota.vercel.app',
+    description: 'Full Stack filter system made with Next (app router)',
+  },
+  {
+    id: crypto.randomUUID(),
     imgUrl: Metaversus.src,
     title: ProjectTitle.Metaversus,
     link: 'https://next-metaverse-theta.vercel.app',
     description: 'Frontend Mentor challenge focus on a Metaverse Landing Page.',
   },
   {
-    id: Date.now(),
+    id: crypto.randomUUID(),
     title: ProjectTitle.Trading,
     link: 'https://trading-website-five.vercel.app',
     imgUrl: TradingSite.src,
     description: 'Landing Page challenge proposed by Frontend-Mentor',
   },
   {
-    id: Date.now(),
+    id: crypto.randomUUID(),
     title: ProjectTitle.URLShortening,
     link: 'https://url-shortening-virid.vercel.app',
     imgUrl: URLShortening.src,
@@ -42,7 +51,7 @@ export const projects: Array<Project> = [
       'Frontend Mentor challenge focus on the integration of a shortened URLs with the shrtcode API.',
   },
   {
-    id: Date.now(),
+    id: crypto.randomUUID(),
     title: ProjectTitle.CountDown,
     link: 'https://launch-countdown-time.netlify.app',
     imgUrl: CountDown.src,
