118a9fe3002d43091f8fc16732d11110d0d1b182
Rabie Menad <rabie.menad@ideine.fr>
2024-06-15 13:36:11 +0200

    ✨ feat - new component letter pullup

--- app/_about/about-header.tsx
+++ app/_about/about-header.tsx
@@ -1,10 +1,11 @@
 import { AvailableBadge } from '@/components/available-badge';
+import { LetterPullUp } from '@/components/ui/letter-pullup';
 
 export const AboutHeader = () => {
   return (
     <div className='flex flex-col gap-3 md:flex-row-reverse md:justify-between'>
       <AvailableBadge />
-      <h2 className='text-xl text-secondary'>FrontEnd Developer</h2>
+      <LetterPullUp words='FrontEnd Developer' />
     </div>
   );
 };
---
+++ components/ui/letter-pullup.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { motion } from 'framer-motion';
+
+type LetterPullUpProps = {
+  className?: string;
+  words: string;
+  delay?: number;
+};
+
+export function LetterPullUp({
+  words,
+  className,
+  delay = 0.05,
+}: LetterPullUpProps) {
+  const letters = words.split('');
+
+  const pullUpVariant = {
+    initial: { y: 100, opacity: 0 },
+    animate: (i: any) => ({
+      y: 0,
+      opacity: 1,
+      transition: { delay: i * delay },
+    }),
+  };
+
+  return (
+    <div className='flex justify-center'>
+      {letters.map((letter, i) => (
+        <motion.h1
+          key={i}
+          initial='initial'
+          animate='animate'
+          variants={pullUpVariant}
+          custom={i}
+          className={cn('text-xl text-secondary', className)}
+        >
+          {letter === ' ' ? <span>&nbsp;</span> : letter}
+        </motion.h1>
+      ))}
+    </div>
+  );
+}
